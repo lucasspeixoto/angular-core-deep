23df40fee31acde3c732bcf8fc99f4a85fdbf04e
Lucas Peixoto <lspeixoto@stefanini.com>
2022-06-14 21:43:25 -0300

    Config lint e prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
