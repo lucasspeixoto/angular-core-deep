a66c34b22535224d3cdf73fbc3e17f7135978e08
Lucas Peixoto <lspeixoto@stefanini.com>
2022-06-16 07:55:45 -0300

    Inclusão server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { COURSES } from 'src/db-data';
+
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-core-deep';
+
+  courses: Course[] = COURSES;
 }
---
+++ src/app/courses/course-card/course-card.component.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from 'src/app/model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css'],
+})
+export class CourseCardComponent implements OnInit {
+  @Input()
+  course!: Course;
+
+  @Input()
+  courseIndex!: number;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
